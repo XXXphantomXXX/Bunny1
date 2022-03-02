53e01c2fc4468e2d9494afb68a73ebbe6db7c52a
XXXphantomXXX <74275394+XXXphantomXXX@users.noreply.github.com>
2022-03-01 20:46:46 -0700

    a1

--- Bunny1/Program.cs
+++ Bunny1/Program.cs
@@ -10,72 +10,66 @@ namespace Bunny1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("what type of operation would you like to preform?");
-            string input = Console.ReadLine();
-
-
-            switch (input.ToLower())
+            bool runner = true;
+            while (runner)
             {
+                Console.WriteLine("what type of operation would you like to preform?");
+                string input = Console.ReadLine();
 
 
-                case "add":
-                case "+":
-                    AddNums();
-                    break;
-                    
-                case "subtract":
-                case "-":
-                    SubtractNums();
-                    break;
+                switch (input.ToLower())
+                {
 
-                case "divide":
-                case "/":
-                    DivideNums();
-                    break;
 
-                case "multiply":
-                case "*":
-                    MultiplyNums();
-                    break;
+                    case "add":
+                    case "+":
+                        AddNums();
+                        break;
 
-                default:
-                    Console.WriteLine("try * + - /");
-                    break;
+                    case "subtract":
+                    case "-":
+                        SubtractNums();
+                        break;
 
+                    case "divide":
+                    case "/":
+                        DivideNums();
+                        break;
+
+                    case "multiply":
+                    case "*":
+                        MultiplyNums();
+                        break;
+
+                    case "close":
+                    case "end":
+                        runner = false;
+                        break;
+
+
+                    default:
+                        Console.WriteLine("try * + - /");
+                        break;
+
+                }
             }
         }
 
-
-        public static void DivideNums()
+        private static void DivideNums()
         {
             Console.WriteLine("Enter first number you want to divide:");
             int NumA1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter second number:");
             int NumB1 = int.Parse(Console.ReadLine());
-            
-                int result = NumA1 / NumB1;
-                Console.WriteLine(result);
+
+            int result = NumA1 / NumB1;
+            Console.WriteLine(result);
             Console.ReadKey();
 
 
         }
 
-        public static void MultiplyNums()
-        {
-            Console.WriteLine("Enter first number you want to Multiply:");
-            int NumA1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter second number:");
-            int NumB1 = int.Parse(Console.ReadLine());
-
-
-            int result = NumA1 * NumB1;
-                Console.WriteLine(result);
-            Console.ReadKey();
-
-
-        }
-
-        public static void SubtractNums()
+        private static void SubtractNums()
         {
             Console.WriteLine("Enter first number you want to subtract:");
             int NumA1 = int.Parse(Console.ReadLine());
@@ -84,13 +78,13 @@ namespace Bunny1
 
 
             int result = NumA1 - NumB1;
-                Console.WriteLine(result);
+            Console.WriteLine(result);
             Console.ReadKey();
-                
-            
+
+
         }
 
-        public static void AddNums()
+        private static void AddNums()
         {
             Console.WriteLine("Enter first number you want to Add:");
             var NumA1 = int.Parse(Console.ReadLine());
@@ -99,10 +93,25 @@ namespace Bunny1
 
 
             int result = NumA1 + NumB1;
-                Console.WriteLine(result);
+            Console.WriteLine(result);
             Console.ReadKey();
-                
-            
+
+
+        }
+
+        private static void MultiplyNums()
+        {
+            Console.WriteLine("Enter first number you want to Multiply:");
+            int NumA1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int NumB1 = int.Parse(Console.ReadLine());
+
+
+            int result = NumA1 * NumB1;
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+
         }
     }
     
